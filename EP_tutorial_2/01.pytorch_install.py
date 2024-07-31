062b2d4248bb48a4583d4897307b6d266b0780ce
jnuho <cactoos555@gmail.com>
2024-07-31 10:13:15 +0900

    .

---
+++ EP_tutorial_2/01.pytorch_install.py
@@ -0,0 +1,7 @@
+# %%
+
+# PyTorch Tutorial 01 ~ 13
+# https://youtu.be/EMXfZB8FVUA?si=rdyy_fRd5iCBWH9h
+
+# Install pytorch locally
+# https://pytorch.org/get-started/locally/
--- EP_tutorial_by_2/01.pytorch_install.py
+++
@@ -1,4 +0,0 @@
-# %%
-# Install pytorch locally
-# https://pytorch.org/get-started/locally/
-# https://youtu.be/EMXfZB8FVUA?si=rdyy_fRd5iCBWH9h
